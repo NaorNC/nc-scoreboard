9f575b1499d063c46f436d450fe110108dd4a57f
NaorNC <81817039+NaorNC@users.noreply.github.com>
2022-03-09 03:28:34 +0200

    Update fxmanifest.lua

--- nc-scoreboard/fxmanifest.lua
+++ nc-scoreboard/fxmanifest.lua
@@ -1,4 +1,8 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+game 'gta5'
+
+description 'nc-scoreboard'
+version '1.0.0'
 
 client_script "config.lua"
 server_script "config.lua"
